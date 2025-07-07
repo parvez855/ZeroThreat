5ca08ac10e0a0b1b2e932bc53b968af4cf6ca255
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-07 15:48:47 +0600

    autonoprefix.js

--- events/autonoprefix.js
+++ events/autonoprefix.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
     client.on('guildMemberUpdate', async (o, n) => {
-        if(o.guild.id !== '1245955247348912188') return;
+        if(o.guild.id !== '1156169360050749471') return;
         let support = await client.guilds.cache.get(`1245955247348912188`)
         let role = support.roles.premiumSubscriberRole
         if (role) {
@@ -8,11 +8,11 @@ module.exports = async (client) => {
             let after = n.roles.cache.has(role.id)
             if (!before && after) {
        await client.db.push(`noprefix_${client.user.id}`,n.id)      
-       await client.channels.cache.get(`1245961700868624466`).send(`Auto Noprefix Added To ${n} With Reason : \`Boosted The Server\``)
+       await client.channels.cache.get(`1290541153996902411`).send(`Auto Noprefix Added To ${n} With Reason : \`Boosted The Server\``)
             }
             if (before && !after) {
                 await client.db.pull(`noprefix_${client.user.id}`,o.id)       
-                await client.channels.cache.get(`1245961725417754685`).send(`Auto Noprefix Removed To ${o} With Reason : \`Removed The Boost\``)
+                await client.channels.cache.get(`1290541153996902411`).send(`Auto Noprefix Removed To ${o} With Reason : \`Removed The Boost\``)
 
                      }
         }      
